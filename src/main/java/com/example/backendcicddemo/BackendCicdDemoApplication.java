f97afe9313a5f450359ca5efd2275090eb7e9832
farihane <redouanefarihane@gmail.com>
2025-08-14 14:18:13 +0200

    Initial commit

---
+++ src/main/java/com/example/backendcicddemo/BackendCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendcicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCicdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCicdDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendcicddemo/BackendCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendcicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCicdDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
