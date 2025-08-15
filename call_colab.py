2d5ba5d4eafbcd421fd694212350f823b51b1b2a
farihane <redouanefarihane@gmail.com>
2025-08-15 19:20:31 +0200

    Test15

--- call_colab.py
+++ call_colab.py
@@ -2,12 +2,12 @@ import requests
 
 GENAI_URL = "https://c55b2ab2da49.ngrok-free.app/hello"
 
-logs_data = {
-    "logs": "Exemple de log de build Jenkins..."
+message_data = {
+    "message": "Build completed successfully ✅"
 }
 
-response = requests.post(GENAI_URL, json=logs_data)
 try:
-    print("Réponse Colab:", response.json())
-except ValueError:
-    print("Erreur JSON, réponse brute:", response.text)
+    response = requests.post(GENAI_URL, json=message_data)
+    print("Server response:", response.json())
+except Exception as e:
+    print("Error sending message:", e)
