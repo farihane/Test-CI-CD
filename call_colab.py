aac7597db36b7fb418ed94db5696bd6c3485742e
farihane <redouanefarihane@gmail.com>
2025-08-15 15:41:19 +0200

    Test15

--- call_colab.py
+++ call_colab.py
@@ -1,6 +1,13 @@
 import requests
 
+# URL publique de ton serveur Colab
 GENAI_URL = "https://e9d60ad9df77.ngrok-free.app/hello"
 
+# Option 1 : GET (récupérer un message simple)
 response = requests.get(GENAI_URL)
-print("Message from Colab:", response.json())
+print("Message from Colab (GET):", response.json())
+
+# Option 2 : POST (envoyer un message)
+data = {"message": "Hello from Jenkins!"}
+response_post = requests.post(GENAI_URL, json=data)
+print("Message from Colab (POST):", response_post.json())
