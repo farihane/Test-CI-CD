8fa93de40583de6ba07094337e246cb2760d5ee4
farihane <redouanefarihane@gmail.com>
2025-08-15 12:57:44 +0200

    Test15

--- src/main/java/com/example/backendcicddemo/HelloController.java
+++ src/main/java/com/example/backendcicddemo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World from Spring Boot!..";
+        return "Hello Redouan from Spring Boot!..";
     }
 }
